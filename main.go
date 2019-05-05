8415cb80ce0a7f5fe6f073de10447d8a29243abf
inksnw <inksnw@gmail.com>
2019-05-05 16:00:23 +0800

    udp args

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	flag.StringVar(&flags.Type, "t", "", "c/s")
 	flag.StringVar(&flags.TCPTun, "tcptun", "", "(client-only) TCP tunnel (laddr1=raddr1,laddr2=raddr2,...)")
+	flag.StringVar(&flags.UDPTun, "ucptun", "", "(client-only) UDP tunnel (laddr1=raddr1,laddr2=raddr2,...)")
 	flag.Parse()
 	local := ":8889"
 	server := "127.0.0.1:8787"
